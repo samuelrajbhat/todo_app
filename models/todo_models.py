3654c929740d4f3c55e2a6d8c76a1bf7ae625198
Samuel Raj Bhat <samuelrajbhat5@gmail.com>
2025-10-31 17:09:17 +0545

    Todo item association with users id made using foreign key

--- models/todo_models.py
+++ models/todo_models.py
@@ -1,7 +1,12 @@
-from sqlalchemy import Column, Integer, String, Boolean, DateTime, Enum
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, Enum, ForeignKey
 from database import Base
 from datetime import datetime
 import enum
+
+from sqlalchemy.orm import relationship, Mapped, mapped_column
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from models.user_models import Users
 
 
 # Enum for todo status
@@ -21,12 +26,17 @@ class PriorityLevel(enum.IntEnum):
 class Todo_Model(Base):
     __tablename__ = "todo"
 
-    id = Column(Integer, primary_key=True, index=True)
-    todo_name = Column(String(100), nullable=False)
-    description = Column(String(250), nullable=True)
-    priority = Column(Enum(PriorityLevel), default=PriorityLevel.LOW)
-    creation_date = Column(DateTime, default=datetime.now)
-    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    todo_name: Mapped[str] = mapped_column(String(100), nullable=False)
+    description: Mapped[str] = mapped_column(String(250), nullable=True)
+    priority: Mapped[PriorityLevel] = mapped_column(Enum(PriorityLevel), default=PriorityLevel.LOW)
+    creation_date: Mapped[datetime] = mapped_column(DateTime, default=datetime.now)
+    updated_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now, onupdate=datetime.now)
     status = Column(Enum(TodoStatus), default=TodoStatus.PENDING)
     is_deleted = Column(Boolean, default=False)
     deleted_at = Column(DateTime, nullable=True)
+
+    # Foreignkey linking to link the table rows to the owner
+
+    owner_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"))
+    owner: Mapped["Users"] = relationship(back_populates="todos")
--- models/user_models.py
+++ models/user_models.py
@@ -1,16 +1,24 @@
 # User models for the user table used for authentication and authorization
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 
 from database import Base
+from sqlalchemy.orm import relationship, Mapped, mapped_column
+from typing import TYPE_CHECKING, List
 
+
+if TYPE_CHECKING:
+    from models.todo_models import Todo_Model
 
 
 class Users(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
     username = Column(String(20), unique=True)
     full_name = Column(String(100))
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
     is_deleted = Column(Boolean, default=False)
+
+# Foreignkey relationship to link users to their todos
+    todos: Mapped[List["Todo_Model"]] = relationship("Todo_Model", back_populates="owner")
