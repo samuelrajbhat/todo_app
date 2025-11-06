ede720717dfa83758ba791c4eaec403e6b64fb7a
Samuel Raj Bhat <samuelrajbhat5@gmail.com>
2025-11-06 13:31:59 +0545

    celery state configuration using postgresql database

--- tasks/celeryconfig.py
+++ tasks/celeryconfig.py
@@ -1,5 +1,6 @@
 broker_url = 'pyamqp://samuel:123456@172.24.189.86//'
-result_backend = 'rpc://'
+# result_backend = 'rpc://'
+result_backend = 'db+postgresql://samuel:123456@localhost/celery_state'
 
 
 
--- tasks/task.py
+++ tasks/task.py
@@ -1,4 +1,5 @@
 from celery import Celery
+import os
 
 # from . import celeryconfig
 
@@ -9,3 +10,14 @@ celery_app.config_from_object('tasks.celeryconfig')
 def add(x,y):
     print(x+y)
     return x + y
+
+
+@celery_app.task
+def write_notification(todo_title: str, current_user: str,  todo_operation:str):
+
+    file_name = "log.txt"
+    mode = "a" if os.path.exists(file_name) else "w"
+
+    with open("log.txt", mode= mode) as notification_file:
+        content = f"todo : {todo_title} {todo_operation} by {current_user} \n"
+        notification_file.write(content)
